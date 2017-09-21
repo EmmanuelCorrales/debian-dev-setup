131dad07077acc0c55de5622d0a1f555fa77d925
Emmanuel Corrales <emmanuelcorrales22@gmail.com>
2017-09-21 16:38:09 +0800

    Setup with JDK and Vim.

--- setup.sh
+++ setup.sh
@@ -1,2 +1,18 @@
 apt-get install build-essentials
 apt-get install git tmux vim
+
+# Initialize git submodules.
+git submodule init
+git submodule update
+
+# Setup javascript development environment.
+echo "Setting up Javascipt development environment."
+sh js-setup.sh
+
+# Setup Java java development environment.
+echo "Setting up JDK..."
+sh ./jdk-ubuntu-setup/jdk8-u101-linux-x64.sh
+
+# Setup vim.
+echo "Setting up vim config and plugins..."
+sh ./vim-setup/setup.sh
