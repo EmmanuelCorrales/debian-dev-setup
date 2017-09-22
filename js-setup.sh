92702c33f92acfd1eca43d4c6cbbca8cb67519a1
Emmanuel Corrales <emmanuelcorrales22@gmail.com>
2017-09-22 15:17:51 +0800

    Fix script. Source the nvm home directory and install the LTS version.

--- js-setup.sh
+++ js-setup.sh
@@ -1,13 +1,16 @@
+#!/bin/bash
+
 #Installer for Javascript Development envronment."
 apt-get update
-apt-get build-essential libssl-dev
+apt-get install build-essential libssl-dev
 
 # Download and run  Node.js installations script.
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | sh
 
-source ~/profile
-# Install Node.js 7.1.0
-nvm install 7.1.0
+source ~/.nvm/nvm.sh
+
+# Install the LTS version
+nvm install --lts
 
 # Show currently installed Node.js version.
 node -v
