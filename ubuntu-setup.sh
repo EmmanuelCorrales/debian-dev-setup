8e53d7022c1f98165cfc72ec5a6eb2fa6b1b5865
Emmanuel Corrales <emmanuelcorrales22@gmail.com>
2017-09-22 15:38:15 +0800

    Do no execute js-setup with sh. Comment out the rest.

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,18 +1,22 @@
+#!/bin/bash
+
 apt-get install build-essential
 apt-get install git tmux vim
 
 # Initialize git submodules.
-git submodule init
-git submodule update
+git submodule init --recursive
+git submodule update --recursive
 
 # Setup javascript development environment.
 echo "Setting up Javascipt development environment."
-sh js-setup.sh
+./js-setup.sh
 
 # Setup Java java development environment.
-echo "Setting up JDK..."
-sh ./jdk-ubuntu-setup/jdk8-u101-linux-x64.sh
+# echo "Setting up JDK..."
+# cd jdk-ubuntu-setup
+# sh jdk8-u101-linux-x64.sh
 
 # Setup vim.
-echo "Setting up vim config and plugins..."
-sh ./vim-setup/setup.sh
+# echo "Setting up vim config and plugins..."
+# cd ../vim-setup
+# sh setup.sh
