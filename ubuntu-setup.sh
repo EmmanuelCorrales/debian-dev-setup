565d95d26f86fb119a3a79b6b6de44fb4c65dcb1
Emmanuel Corrales <emmanuelcorrales22@gmail.com>
2017-09-22 16:36:34 +0800

    Fix setting up vim.

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -4,19 +4,20 @@ apt-get install build-essential
 apt-get install git tmux vim
 
 # Initialize git submodules.
-git submodule init --recursive
-git submodule update --recursive
+git submodule init
+git submodule update
 
 # Setup javascript development environment.
-echo "Setting up Javascipt development environment."
+echo "Setting up Javascipt development environment..."
 ./js-setup.sh
+
+# Setup vim.
+echo "Setting up vim config and plugins..."
+cd vim-setup
+./setup.sh
+cd ..
 
 # Setup Java java development environment.
 # echo "Setting up JDK..."
 # cd jdk-ubuntu-setup
 # sh jdk8-u101-linux-x64.sh
-
-# Setup vim.
-# echo "Setting up vim config and plugins..."
-# cd ../vim-setup
-# sh setup.sh
