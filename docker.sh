6794718a10ae5fb121a0502b91a34127408be9f9
Emmanuel Corrales <contact@emcorrales.com>
2019-01-28 22:06:38 -0800

    Install Docker completion for the bash shell.

--- docker.sh
+++ docker.sh
@@ -2,6 +2,12 @@
 
 if [ $(uname) = "Darwin" ]; then
   brew cask install docker
+
+  # Docker completion.
+  etc=/Applications/Docker.app/Contents/Resources/etc
+  ln -s $etc/docker.bash-completion $(brew --prefix)/etc/bash_completion.d/docker
+  ln -s $etc/docker-machine.bash-completion $(brew --prefix)/etc/bash_completion.d/docker-machine
+  ln -s $etc/docker-compose.bash-completion $(brew --prefix)/etc/bash_completion.d/docker-compose
 elif [ $(uname) = "Linux" ]; then
   # SETUP REPOSITORY for Ubuntu.
   sudo apt-get update
