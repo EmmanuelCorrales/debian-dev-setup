7394e1b67d0b45467c75cc324d98dd2f8bdeb640
Emmanuel Corrales <contact@emcorrales.com>
2019-02-04 15:38:54 -0800

    Export JAVA_HOME.

--- jdk-setup.bash
+++ jdk-setup.bash
@@ -6,15 +6,17 @@ if [ $(uname) = "Linux" ]; then
   sudo apt-get install oracle-java8-installer
   sudo apt-get install oracle-java8-set-default
 
-  java -version
-  javac -version
-  javaws -eersion
 elif [ $(uname) = "Darwin" ]; then
   brew tap caskroom/versions
   brew cask install java8
+  export JAVA_HOME=$(/usr/libexec/java_home)
 else
   echo "OS not supported."
   exit 0
 fi
+
+java -version
+javac -version
+javaws -eersion
 
 echo "You may need to logout or restart to be able to apply changes to the JAVA_HOME."
